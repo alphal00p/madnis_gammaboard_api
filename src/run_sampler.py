2051c7ab93d0e4a783112cd8e0ceae7ac2197772
Cedric Sigrist <cedric@sigrist.ch>
2026-05-20 14:41:11 +0200

    Make MADNIS sampler entrypoint installable

--- src/run_sampler.py
+++ src/run_sampler.py
@@ -2,4 +2,9 @@ from madnis_sampler import MadnisSampler
 from gammaboard_process import run_sampler
 
 
-run_sampler(MadnisSampler)
+def main() -> None:
+    run_sampler(MadnisSampler)
+
+
+if __name__ == "__main__":
+    main()
